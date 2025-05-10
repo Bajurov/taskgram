b6fee555ba564b807a0a2d379945f65887b3c4a6
Igor <bajurov@mail.ru>
2025-05-10 11:22:46 +0300

    Исправлена ошибка отображения задачи

--- src/views/TaskView.vue
+++ src/views/TaskView.vue
@@ -2,14 +2,25 @@
   <div style="background: #ff0; color: #000; font-size: 22px; padding: 20px;">
     TASK VIEW IS RENDERED<br>
     route id: {{ route.params.id }}<br>
-    user: {{ userStore.currentUser }}
+    user: {{ userStore.currentUser }}<br>
+    tasks loading: {{ tasksStore.loading }}<br>
+    tasks error: {{ tasksStore.error }}<br>
+    tasks: {{ tasksStore.tasks }}<br>
+    tasks ids: {{ tasksStore.tasks.map(t => t.id) }}
   </div>
 </template>
 
 <script setup lang="ts">
 import { useRoute } from 'vue-router';
 import { useUserStore } from '../store/user';
+import { useTasksStore } from '../store/tasks';
+import { onMounted } from 'vue';
 
 const route = useRoute();
 const userStore = useUserStore();
+const tasksStore = useTasksStore();
+
+onMounted(() => {
+  tasksStore.fetchTasks();
+});
 </script>
