872a7114ca32ee0ba76f6962f1fa8cb733d82bcb
Igor <bajurov@mail.ru>
2025-05-10 11:21:15 +0300

    Исправлена ошибка отображения задачи

--- src/views/TaskView.vue
+++ src/views/TaskView.vue
@@ -1,5 +1,15 @@
 <template>
   <div style="background: #ff0; color: #000; font-size: 22px; padding: 20px;">
-    TASK VIEW IS RENDERED
+    TASK VIEW IS RENDERED<br>
+    route id: {{ route.params.id }}<br>
+    user: {{ userStore.currentUser }}
   </div>
 </template>
+
+<script setup lang="ts">
+import { useRoute } from 'vue-router';
+import { useUserStore } from '../store/user';
+
+const route = useRoute();
+const userStore = useUserStore();
+</script>
